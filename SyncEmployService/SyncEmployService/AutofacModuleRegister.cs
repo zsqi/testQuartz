55927ef420442344c05b0c257d565bc84ea595a7
1214565968@qq.com <1214565968@qq.com>
2022-08-15 16:54:06 +0800

    1

---
+++ SyncEmployService/SyncEmployService/AutofacModuleRegister.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncEmployService
+{
+    public class AutofacModuleRegister : Autofac.Module
+    {
+        //重写Autofac管道Load方法，在这里注册注入
+        protected override void Load(ContainerBuilder builder)
+        {
+            //程序集注入业务服务
+            var IAppServices = Assembly.Load("Application");
+            var AppServices = Assembly.Load("Application");
+            //根据名称约定（服务层的接口和实现均以Service结尾），实现服务接口和服务实现的依赖
+            builder.RegisterAssemblyTypes(IAppServices, AppServices)
+              .Where(t => t.Name.EndsWith("Service"))
+              .AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(IAppServices, AppServices)
+             .Where(t => t.Name.EndsWith("Repository"))
+             .AsImplementedInterfaces();
+        }
+    }
+}
